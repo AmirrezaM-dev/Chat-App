6602e6e1d583291fdb88ae749fbf4c7fb15e030a
Alex Noah <67859130+AmirrezaM-dev@users.noreply.github.com>
2024-04-26 14:48:29 +0400

    [Bug Fix]

--- server/server.js
+++ server/server.js
@@ -40,18 +40,13 @@ server.once("close", function () {
 
 	connectDB()
 
-	try {
-		app.use((req, res, next) => {
-			console.log(req.headers.cookie)
-			console.log(req.cookies)
-			next()
-		})
-		app.use(cookieParser())
-	} catch (e) {
-		console.log(e)
-	}
-
 	app.use(cors(CORS))
+	app.use((req, res, next) => {
+		console.log(req.headers.cookie)
+		console.log(req.cookies)
+		next()
+	})
+	app.use(cookieParser())
 
 	app.use(express.json())
 	app.use(express.urlencoded({ extended: false }))
